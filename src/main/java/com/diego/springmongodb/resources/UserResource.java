26ed47e54a36d9c90f9a3aba7b84c375ce27d054
desferreira <diego.ferreira@justa.com.vc>
2020-03-10 22:47:39 -0300

    :sparkles: Implementa update, delete e insert na classe UserService
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

--- src/main/java/com/diego/springmongodb/resources/UserResource.java
+++ src/main/java/com/diego/springmongodb/resources/UserResource.java
@@ -3,13 +3,12 @@ package com.diego.springmongodb.resources;
 import com.diego.springmongodb.domain.User;
 import com.diego.springmongodb.dto.UserDTO;
 import com.diego.springmongodb.services.UserService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +33,32 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping(value = "/insert")
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}/delete")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        User obj = service.findById(id);
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}/update")
+    public ResponseEntity<User> update(@RequestBody UserDTO objDto, @PathVariable String id){
+
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+
+
     }
 
 
--- src/main/java/com/diego/springmongodb/services/UserService.java
+++ src/main/java/com/diego/springmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.diego.springmongodb.services;
 
 import com.diego.springmongodb.domain.User;
+import com.diego.springmongodb.dto.UserDTO;
 import com.diego.springmongodb.repositories.UserRepository;
 import com.diego.springmongodb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,31 @@ public class UserService {
 
     public void insert(User obj){
         repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+    public void delete(String id){
+        Optional<User> user = repository.findById(id);
+        if (user == null){
+            throw new ResourceNotFoundException(id);
+        }
+        repository.delete(user.get());
+    }
+
+    public User update(User obj){
+        Optional<User> newObj = repository.findById(obj.getId());
+        updateData(newObj.get(), obj);
+        return repository.save(newObj.get());
+    }
+
+    private void updateData(User newObj, User obj) {
+
+        newObj.setEmail(obj.getEmail());
+        newObj.setName(obj.getName());
+
     }
 
 }
